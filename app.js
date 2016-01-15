35ccc3594e4bca62161f65720a9f05a66fa46cb9
Antonio Cicero <aixoniarutad@gmail.com>
2016-01-15 03:03:56 -0500

    Reduce file size and increase speed

--- app.js
+++ app.js
@@ -1,47 +1,47 @@
 (function() {
+	// Start Game
+	document.getElementById('start').addEventListener('click', function(){
+		console.time('timelapse');
+		var gameAmount = document.getElementById('game_amount').value,
+			gameSize = document.getElementById('game_size').value,
+			gameResult = [0, 0, 0, 0, 0, 0],
+			winning = generate(gameSize);
+			document.getElementById('jackpot').innerHTML=winning;
+			document.getElementById('user_wins').innerHTML="";
 
-	var startGame = function(gameAmount, gameSize, fn){
-		var gameResult = [0, 0, 0, 0, 0, 0];
-
-		for(var i=0; i < gameAmount; i++){
-			var playerNumbers = generate(gameSize);
-			var winningNumbers = generate(gameSize);
-			var matched = checkNumbers(playerNumbers, winningNumbers, 0);
+		for(var i=0; i < gameAmount; i++)
+			startGame(winning, function(matched){
 				gameResult[matched]++;
-		}
-		return fn(gameResult);
-	}
+			});
 
+		return populate(gameResult);
+	});
 	var generate = function(count) {
-		var numbers = new Array(count);
+		var numbers=[];
 		for(var i=0; i < count; i++)
-			numbers[i] = Math.floor((Math.random() * 69) + 1);
+			numbers[i]=Math.floor((Math.random() * 69) + 1);
 		return numbers;
 	}
-
-	var checkNumbers = function(player, winning, total) {
-		for(i in player){
-			for(n in winning){
-				if(player[i]===winning[n]) {
-					player.splice(i, 1);
-					winning.splice(n, 1);
-					return checkNumbers(player, winning, ++total);
-				}
-			}
-		}
-		return total;
+	var populate = function(result){
+		for(i in result)
+			document.getElementById('match-'+i).innerHTML = result[i];
+			console.timeEnd('timelapse');
 	}
+	var startGame = function(winning, cb){
+		var player = generate(winning.length);
+		var match = 0;
 
-	// Start
-	document.getElementById('start').addEventListener('click', function() {
-		var gameAmount = document.getElementById('game_amount').value;
-		var gameSize = document.getElementById('game_size').value;
-
-		startGame(gameAmount, gameSize, function(result) {
-			for(i in result)
-				document.getElementById('match-'+i).innerHTML = result[i];
-
-			return document.getElementById('total_played').innerHTML = gameAmount;
-		});
-	});
+		for (var i = 0, len = winning.length; i < len; i++) {
+		    if (player.indexOf(winning[i]) !== -1) {
+		        match++;
+		        player[player.indexOf(winning[i])]=winning[i].toString();
+		    }
+		}
+		if(match>=5){
+			var game = document.createElement('div');
+				game.innerHTML=player;
+				document.getElementById('user_wins').appendChild(game);
+		}
+		return cb(match);
+	}
 })();
